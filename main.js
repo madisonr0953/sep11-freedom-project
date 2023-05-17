b3864c411ec4846f7280ece5d8ff325648691133
Madison Reyes <Madisonr0953@hstat.org>
2023-05-17 08:47:28 -0400

    finished with project

--- main.js
+++ main.js
@@ -1,76 +1,142 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { FirstPersonControls } from 'https://cdn.skypack.dev/three@0.136/examples/jsm/controls/FirstPersonControls.js';
-
-var person
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+scene.background =  new THREE.Color(0x0000FF)
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const floorGeometry = new THREE.PlaneGeometry(10, 10);
-const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+const textures = new THREE.TextureLoader().load('spacer.jpg')
+scene.background = textures;
+
+camera.position.z = 5
+camera.position.y = 0.5
+document.body.appendChild(renderer.domElement);
+var directionalLight = new THREE.DirectionalLight({ color: 0xFFFFFF, intensity: 100})
+directionalLight.position.set(0,1,0)
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight);
+
+
+
+
+const floorGeometry = new THREE.PlaneGeometry(100, 100);
+const floorMaterial = new THREE.MeshBasicMaterial({ color : 0x013220});
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
 floor.rotation.x = -Math.PI / 2; // Rotate the floor to be horizontal
 scene.add(floor);
 
+var cubeText = new THREE.TextureLoader().load('brick.jpg')
 
-// Create maze walls
-const wallGeometry = new THREE.BoxGeometry(1, 2, 1);
-const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x333333 });
+
+const geometry = new THREE.BoxGeometry(15, 10, 1);
+const material = new THREE.MeshBasicMaterial({ map: cubeText });
+const cube = new THREE.Mesh(geometry, material);
+
+cube.position.x = 5;
+cube.position.y = 0.5;
+cube.position.z = -22.5;
+
+scene.add(cube);
+
+var cubeText1 = new THREE.TextureLoader().load('brick.jpg')
+
+const geometry1 = new THREE.BoxGeometry(15, 10, 1);
+const material1 = new THREE.MeshBasicMaterial({ map: cubeText1 });
+const cube1 = new THREE.Mesh(geometry1, material1);
+
+cube1.position.x = 5;
+cube1.position.y = 0.5;
+cube1.position.z = 10;
+
+scene.add(cube1);
+
+const geometry2 = new THREE.BoxGeometry(1, 10, 32);
+const material2 = new THREE.MeshBasicMaterial({ map: cubeText1 });
+const cube2 = new THREE.Mesh(geometry2, material2);
+
+cube2.position.x = 12.5;
+cube2.position.y = 5;
+cube2.position.z = -6;
+scene.add(cube2);
+
+
+
+
+var wallText = new THREE.TextureLoader().load('brick.jpg')
+const wallGeometry = new THREE.BoxGeometry(1, 4, 1);
+const wallMaterial = new THREE.MeshBasicMaterial({ map: wallText });
 const walls = [];
-// Define your maze structure here, e.g.:
+
 const maze = [
-    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
-    [1, 1, 0, 0, 0, 1, 1, 1, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
-    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-    [1, 0, 0, 1, 1, 0, 0, 1, 1, 1],
-    [1, 1, 0, 0, 0, 1, 0, 1, 1, 1],
-    [1, 0, 0, 1, 0, 1, 0, 1, 1, 1],
-    [1, 0, 1, 1, 0, 0, 0, 1, 1, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 1,1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 1,1, 0, 0, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 0, 0,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 0, 0, 1,1, 0, 0, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 0, 0,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 0, 0, 1,1, 0, 0, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 0, 0,1, 0, 0, 0, 0, 0, 0, 1, 0, 0,0, 0, 0, 1,1, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 0, 0, 0, 0, 0,1, 0, 0, 0, 0, 0, 0, 1, 0, 0,0, 0, 0, 1,1, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 0, 0, 0, 0, 0,1, 1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,1, 1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,1, 1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,1, 1, 1, 1, 1, 0, 0, 0, 1, 0,0, 0, 0, 0,0, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,1, 1, 1, 1, 1, 0, 0, 0, 1, 0,0, 0, 0, 0,0, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 0,0, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 0, 0, 0, 0, 0, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0,0, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0,0, 1, 1, 0, 0, 0, 0, 0, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 1, 1, 0, 0, 0, 0, 0, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 0, 0, 0, 0, 1, 1, 1, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0,0, 0, 0, 0, 0, 1, 1, 1, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 0,0, 0, 0, 0,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 0,0, 0, 0, 0,0, 0, 0, 0, 0, 0, 1, 1, 0,0, 0, 0, 0,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 0,0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0,0, 1, 1, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 1, 0,0, 1, 1, 1,1, 1, 1, 1, 0, 0, 0, 0, 0,0, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 0, 0, 1, 1, 1, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 0,0, 1, 1, 1,1, 1, 1, 1, 1, 1, 0, 0, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 0, 0,0, 0, 0, 0, 0, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 1, 1, 0, 0, 1,1, 1, 1, 1,],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1,],
+
+
 
 ];
 for (let i = 0; i < maze.length; i++) {
     for (let j = 0; j < maze[i].length; j++) {
         if (maze[i][j] === 1) {
             const wall = new THREE.Mesh(wallGeometry, wallMaterial);
-            wall.position.set(j - Math.floor(maze[i].length / 2), 1, i - Math.floor(maze.length / 2));
+            wall.position.set(j - Math.floor(maze[i].length / 1), 1, i - Math.floor(maze.length / 1.5));
             scene.add(wall);
             walls.push(wall);
         }
     }
 }
-const light = new THREE.PointLight(0xffffff);
-light.position.set(0, 10, 0);
-scene.add(light);
 
-camera.position.set(8, 1.5, 5);
-camera.lookAt(0,0,0);
 
 
 
 var controls = new FirstPersonControls( camera, renderer.domElement )
-controls.movementSpeed = 0.1;
-controls.lookSpeed = 0.01
-
-
-// Game loop
-const animate = () => {
-    requestAnimationFrame(animate);
-
-    // Update game logic
-    controls.update(0.3)
+controls.movementSpeed = 0.1
+controls.lookVertical = false;
+controls.lookSpeed = 0.005
 
 
 
+
+function drawScene(){
     renderer.render(scene, camera);
-};
-animate();
+    requestAnimationFrame(drawScene);
+    controls.update(0.3)
+    checkCollision();
+
+}
+
+drawScene();
